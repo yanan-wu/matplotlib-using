071e118b796113570e1ba5352ffca458a3c67cca
yanan-wu <wuyn1760@aliyun.com>
2019-08-20 21:03:03 +0800

    2019082001_roe bar

--- bar/bar_pic.py
+++ bar/bar_pic.py
@@ -7,7 +7,11 @@ from matplotlib.font_manager import FontProperties
 font_set = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=12)
 
 
-def read_csv():
+def per_share():
+    """
+    每股收益-年份柱状图
+    :return:
+    """
     csv_data = pd.read_csv('../data/000988_data.csv', index_col=0, names=[1, 2, 3, 4, 5, 6, 7, 8, 9])
     x = csv_data.iloc[:1].values.tolist()[0][::-1]
     y = csv_data.iloc[1:2].values.tolist()[0][::-1]
@@ -20,7 +24,28 @@ def read_csv():
     plt.legend()
     plt.xlabel('年份', fontproperties=font_set)
     plt.ylabel('基本每股收益(元)', fontproperties=font_set)
+    plt.savefig('D:/share.png')
     plt.show()
 
 
-read_csv()
+def roe_pic():
+    """
+    净资产收益率-年份柱状图
+    :return:
+    """
+    csv_data = pd.read_csv('../data/000988_data.csv', index_col=0, names=[1, 2, 3, 4, 5, 6, 7, 8, 9])
+    x = csv_data.iloc[:1].values.tolist()[0][::-1]
+    y = csv_data.iloc[4:5].values.tolist()[0][::-1]
+    plt.bar(range(9), y, width=0.5)
+    plt.plot(range(9), y, color='red')
+    plt.xticks(range(9), x, rotation=20)
+
+    plt.title('华工科技历年 ROE 变化图', fontproperties=font_set)
+    plt.legend()
+    plt.xlabel('年份', fontproperties=font_set)
+    plt.ylabel('ROE', fontproperties=font_set)
+    plt.savefig('D:/roe.png')
+    plt.show()
+
+
+per_share()
