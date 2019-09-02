682b320bf21432874ebb0fd848bb536b02e7486b
yanan-wu <wuyn1760@aliyun.com>
2019-09-02 22:52:49 +0800

    2019090201_roe bar

--- bar/bar_pic.py
+++ bar/bar_pic.py
@@ -33,14 +33,14 @@ def roe_pic():
     净资产收益率-年份柱状图
     :return:
     """
-    csv_data = pd.read_csv('../data/000988_data.csv', index_col=0, names=[1, 2, 3, 4, 5, 6, 7, 8, 9])
+    csv_data = pd.read_csv('../data/002281_data.csv', index_col=0, names=[1, 2, 3, 4, 5, 6, 7, 8, 9])
     x = csv_data.iloc[:1].values.tolist()[0][::-1]
-    y = csv_data.iloc[4:5].values.tolist()[0][::-1]
+    y = csv_data.iloc[1:2].values.tolist()[0][::-1]
     plt.bar(range(9), y, width=0.5)
     plt.plot(range(9), y, color='red')
     plt.xticks(range(9), x, rotation=20)
 
-    plt.title('华工科技历年 ROE 变化图', fontproperties=font_set)
+    plt.title('光迅科技历年 ROE 变化图', fontproperties=font_set)
     plt.legend()
     plt.xlabel('年份', fontproperties=font_set)
     plt.ylabel('ROE(%)', fontproperties=font_set)
@@ -48,4 +48,4 @@ def roe_pic():
     plt.show()
 
 
-per_share()
+roe_pic()
